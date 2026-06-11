79c055d81c04e0882018bd48ba9449e114c81e24
Lakshmi-Pramode <lakshmipramode2027@cs.sjcetpalai.ac.in>
2026-06-11 20:27:37 +0530

    Fix CORS for Vercel

--- server/server.js
+++ server/server.js
@@ -22,7 +22,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://qure-six.vercel.app",
+    ],
     credentials: true,
   })
 );
